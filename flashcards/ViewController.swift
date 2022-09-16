dda6c86c174321bd7f04e3afa8dbcedd403cbe5b
angram03 <106850456+angram03@users.noreply.github.com>
2022-09-15 17:29:54 -0700

    Initial Commit

---
+++ flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashcards
+//
+//  Created by Angelica Ramirez on 9/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
